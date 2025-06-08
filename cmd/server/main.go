fbc3caff968c3247c3e7d540ff67f66679c50c27
naimulh247 <naimulh247@gmail.com>
2025-06-08 15:46:47 -0400

    Implement server setup with health check and middleware configuration
    - downgrade to fiber v2 from v3 due to conflicting docs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 
-	fmt.Println("Starting the server...")
+	log.Println("Setting up server...")
+
+	// create an fiber app
+	app := fiber.New(fiber.Config{
+		AppName: "FlipFlop API",
+	})
+
+	// middlewares
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // allow all origins
+		AllowMethods: "GET,POST,PUT,DELETE,PATCH",
+		AllowHeaders: "Origin,Content-Type,Accept,Authorization",
+	}))
+
+	// health check
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  "UP",
+			"message": "Server is running smoothly!",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port ==  "" {
+		port = "8080" // default port
+	}
+
+	log.Printf("Starting server on port %s...\n", port)
+	// start the server
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Error starting server: %v\n", err)
+	}
+
 }
